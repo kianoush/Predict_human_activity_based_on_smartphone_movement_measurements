1601cce5f88b5879232bf051c0f0876a47ea245d
Kian1212 <k.karimipor@gmail.com>
2019-09-10 20:19:30 -0700

    load data and split it to train, test and valid

--- Smart_phone_MLP.py
+++ Smart_phone_MLP.py
@@ -5,7 +5,7 @@ Smart Phone
 import numpy as np
 import torch
 import pandas as pd
-from sklearn import model_selection
+from sklearn.model_selection import train_test_split
 import os
 
 
@@ -13,10 +13,21 @@ import os
 Load data
 """
 
-print(os.listdir())
+raw_data = pd.read_csv('smartphone_activity_dataset.csv')
+
+data = raw_data.iloc[:, :561]
+lables = raw_data.iloc[:, 561]
+
+data = data.values
+lables = lables.values
 
 
+"""
+Split Train, Test and Valid
+"""
 
+x_train, X_test, y_train, Y_test = train_test_split(data, lables, test_size=0.2, shaffle=True)
+X_train, X_valid, Y_train, Y_valid = train_test_split(x_train, y_train, test_size=0.2, shaffle=True)
 
 
 
