611501a72d76b5f0f803606e4df7ad65b40268c3
Kian1212 <k.karimipor@gmail.com>
2019-09-10 19:07:30 -0700

    Initial commit

---
+++ Smart_phone_MLP.py
@@ -0,0 +1,24 @@
+"""
+Smart Phone
+"""
+
+import numpy as np
+import torch
+import pandas as pd
+from sklearn import model_selection
+import os
+
+
+"""
+Load data
+"""
+
+print(os.listdir())
+
+
+
+
+
+
+
+print("END!!!")
